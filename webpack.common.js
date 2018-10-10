bcb68012bbd50477082ded0b2366e916c7bb0521
Elias E. del Real <pixelshader@outlook.com>
2018-10-09 20:23:23 -0400

    Initial setup of webpack and simple 3JS example.

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.rs$/,
+        use: [{
+          loader: 'wasm-loader'
+        }, {
+          loader: 'rust-native-wasm-loader',
+          options: {
+            gc: true,
+            release: true
+          }
+        }]
+      }
+    ]
+  }
+}
