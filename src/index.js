8591c5e72e9757fcd501421a1c0dfaf982c02145
Elias E. del Real <pixelshader@outlook.com>
2018-10-09 20:53:57 -0400

    Add keyboard control.

--- src/index.js
+++ src/index.js
@@ -8,19 +8,33 @@ renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 )
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
+var material = new THREE.MeshBasicMaterial( { color: 0x550000} )
 var cube = new THREE.Mesh( geometry, material )
 scene.add( cube )
 
 camera.position.z = 5
 
+var xRotationDelta = 0
+var yRotationDelta = 0
+
 var animate = function () {
   requestAnimationFrame( animate )
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  cube.rotation.x += xRotationDelta
+  cube.rotation.y += yRotationDelta
 
   renderer.render( scene, camera )
 };
 
 animate()
+
+// Keyboard Input Code.
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+      // Left arrow.
+        yRotationDelta -= 0.001
+    } else if(event.keyCode == 39) {
+      // Right arrow.
+        yRotationDelta += 0.001
+    }
+});
